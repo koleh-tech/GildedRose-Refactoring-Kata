eecedfe0fed9a627156905356a18b5433c5f76c6
Koleh David <koleh.david@aerometrex.com.au>
2025-02-06 16:40:31 +1030

    rm redundant

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,28 +1,15 @@
+import unittest
+
 from gilded_rose import Item, GildedRose
-from approvaltests.combination_approvals import verify_all_combinations
 
 
-def test_update_quality():
-    input_names = [
-        "foo",
-        "Aged Brie",
-        "Backstage passes to a TAFKAL80ETC concert",
-        "Sulfuras, Hand of Ragnaros",
-    ]
-    input_qualities = [-1, 0, 1, 49, 50, 51]
-    input_sell_ins = [-1, 0, 1, 10, 11, 12]
-
-    verify_all_combinations(
-        do_stuff,
-        [
-            input_names,
-            input_sell_ins,
-            input_qualities,
-        ],
-    )
+class GildedRoseTest(unittest.TestCase):
+    def test_foo(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("fixme", items[0].name)
 
 
-def do_stuff(name, sell_in, quality):
-    item = Item(name, sell_in, quality)
-    GildedRose().update_quality([item])
-    return str(item)
+if __name__ == "__main__":
+    unittest.main()
