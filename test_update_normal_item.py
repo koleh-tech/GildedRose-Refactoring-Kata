ac15f88df4e51b12e2930f0ac66a56e86c2e98d2
Koleh David <koleh.david@aerometrex.com.au>
2025-02-17 08:28:22 +1030

    r cleanup

--- test_update_normal_item.py
+++ test_update_normal_item.py
@@ -9,11 +9,20 @@ def run_update_item(normal_item):
 
 
 def test_update_conjured_item():
-    # args: ('foo', -1, 51) => 'foo, -2, 49'
     normal_item = Item("Normal item 1232112", -1, 5)
     normal_item_diff = normal_item.quality - run_update_item(normal_item).quality
 
     conjured_item = Item("Conjured", -1, 5)
+    conjured_item_diff = conjured_item.quality - run_update_item(conjured_item).quality
+
+    assert normal_item_diff == conjured_item_diff / 2
+
+
+def test_update_conjured_item2():
+    normal_item = Item("Normal item 1232112", -2, -1)
+    normal_item_diff = normal_item.quality - run_update_item(normal_item).quality
+
+    conjured_item = Item("Conjured", -2, -1)
     conjured_item_diff = conjured_item.quality - run_update_item(conjured_item).quality
 
     assert normal_item_diff == conjured_item_diff / 2
