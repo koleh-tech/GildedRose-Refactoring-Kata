3be4853d0b2d8659309ebd18f93432aa77d05e4b
Koleh David <koleh.david@aerometrex.com.au>
2025-02-17 08:39:41 +1030

    r move

--- test_update_conjured_item.py
+++ test_update_conjured_item.py
@@ -2,41 +2,22 @@ from copy import deepcopy
 from gilded_rose import Item, update_normal_item
 
 
-def get_quality_diff(normal_item):
-    input = normal_item
-
-    output_item = deepcopy(input)
-    update_normal_item(output_item)
-
-    normal_item_diff = normal_item.quality - input.quality
-    return normal_item_diff
-
-
 def test_quality_greater_than_0():
-    normal_item = Item("Normal item 1232112", -1, 5)
-    normal_item_diff = get_quality_diff(normal_item)
+    normal_diff = get_quality_diff(Item("Normal item 1232112", -1, 5))
+    conjured_diff = get_quality_diff(Item("Conjured", -1, 5))
 
-    conjured_item = Item("Conjured", -1, 5)
-
-    conjured_item_diff = thing(conjured_item)
-
-    assert normal_item_diff == conjured_item_diff / 2
-
-
-def thing(conjured_item):
-    output_item = deepcopy(conjured_item)
-    update_normal_item(output_item)
-
-    conjured_item_diff = conjured_item.quality - conjured_item.quality
-    return conjured_item_diff
+    assert normal_diff > 0
+    assert conjured_diff > 0
+    assert normal_diff == conjured_diff / 2
 
 
 def test_update_less_than_0():
-    normal_item = Item("Normal item 1232112", -2, -1)
-    normal_item_diff = get_quality_diff(normal_item)
+    normal_item_diff = get_quality_diff(Item("Normal item 1232112", -2, -1))
+    conjured_diff = get_quality_diff(Item("Conjured", -2, -1))
+    assert normal_item_diff == conjured_diff / 2
 
-    conjured_item = Item("Conjured", -2, -1)
 
-    conjured_item_diff = thing(conjured_item)
-
-    assert normal_item_diff == conjured_item_diff / 2
+def get_quality_diff(input_item: Item):
+    output_item = deepcopy(input_item)
+    update_normal_item(output_item)
+    return input_item.quality - output_item.quality
