36fd90eb488bb03796bc367cd7d7da81b29380d0
Koleh David <koleh.david@aerometrex.com.au>
2025-02-17 08:24:40 +1030

    r extract funcs

--- test_update_normal_item.py
+++ test_update_normal_item.py
@@ -2,16 +2,18 @@ from copy import deepcopy
 from gilded_rose import Item, update_normal_item
 
 
+def run_update_item(normal_item):
+    output_item = deepcopy(normal_item)
+    update_normal_item(output_item)
+    return output_item
+
+
 def test_update_conjured_item():
     # args: ('foo', -1, 51) => 'foo, -2, 49'
     normal_item = Item("Normal item 1232112", -1, 5)
-    output_item = deepcopy(normal_item)
-    update_normal_item(output_item)
-    normal_item_diff = normal_item.quality - output_item.quality
+    normal_item_diff = normal_item.quality - run_update_item(normal_item).quality
 
-    normal_item = Item("Conjured", -1, 5)
-    output_item = deepcopy(normal_item)
-    update_normal_item(output_item)
-    conjured_item_diff = normal_item.quality - output_item.quality
+    conjured_item = Item("Conjured", -1, 5)
+    conjured_item_diff = conjured_item.quality - run_update_item(conjured_item).quality
 
     assert normal_item_diff == conjured_item_diff / 2
