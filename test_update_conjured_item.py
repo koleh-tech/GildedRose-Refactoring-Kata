04a0928ab56f17460959ea2776c1b4a21bff17a4
Koleh David <koleh.david@aerometrex.com.au>
2025-02-17 08:32:22 +1030

    renmae

--- test_update_conjured_item.py
+++ test_update_conjured_item.py
@@ -8,9 +8,14 @@ def run_update_item(normal_item):
     return output_item
 
 
-def test_update_conjured_item():
-    normal_item = Item("Normal item 1232112", -1, 5)
+def get_quality_diff(normal_item):
     normal_item_diff = normal_item.quality - run_update_item(normal_item).quality
+    return normal_item_diff
+
+
+def test_quality_greater_than_0():
+    normal_item = Item("Normal item 1232112", -1, 5)
+    normal_item_diff = get_quality_diff(normal_item)
 
     conjured_item = Item("Conjured", -1, 5)
     conjured_item_diff = conjured_item.quality - run_update_item(conjured_item).quality
@@ -18,9 +23,9 @@ def test_update_conjured_item():
     assert normal_item_diff == conjured_item_diff / 2
 
 
-def test_update_conjured_item2():
+def test_update_less_than_0():
     normal_item = Item("Normal item 1232112", -2, -1)
-    normal_item_diff = normal_item.quality - run_update_item(normal_item).quality
+    normal_item_diff = get_quality_diff(normal_item)
 
     conjured_item = Item("Conjured", -2, -1)
     conjured_item_diff = conjured_item.quality - run_update_item(conjured_item).quality
