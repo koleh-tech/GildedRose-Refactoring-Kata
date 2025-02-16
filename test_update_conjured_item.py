7c8a3fcf73a7ce561316c53f7de8bf9f7bc47eb9
Koleh David <koleh.david@aerometrex.com.au>
2025-02-17 08:33:55 +1030

    r inline vars

--- test_update_conjured_item.py
+++ test_update_conjured_item.py
@@ -3,11 +3,12 @@ from gilded_rose import Item, update_normal_item
 
 
 def get_quality_diff(normal_item):
-    thing = input = normal_item
+    input = normal_item
+
     output_item = deepcopy(input)
     update_normal_item(output_item)
 
-    normal_item_diff = normal_item.quality - thing.quality
+    normal_item_diff = normal_item.quality - input.quality
     return normal_item_diff
 
 
@@ -16,11 +17,11 @@ def test_quality_greater_than_0():
     normal_item_diff = get_quality_diff(normal_item)
 
     conjured_item = Item("Conjured", -1, 5)
-    thing = input = conjured_item
-    output_item = deepcopy(input)
+
+    output_item = deepcopy(conjured_item)
     update_normal_item(output_item)
 
-    conjured_item_diff = conjured_item.quality - thing.quality
+    conjured_item_diff = conjured_item.quality - conjured_item.quality
 
     assert normal_item_diff == conjured_item_diff / 2
 
@@ -30,10 +31,10 @@ def test_update_less_than_0():
     normal_item_diff = get_quality_diff(normal_item)
 
     conjured_item = Item("Conjured", -2, -1)
-    thing = input = conjured_item
-    output_item = deepcopy(input)
+
+    output_item = deepcopy(conjured_item)
     update_normal_item(output_item)
 
-    conjured_item_diff = conjured_item.quality - thing.quality
+    conjured_item_diff = conjured_item.quality - conjured_item.quality
 
     assert normal_item_diff == conjured_item_diff / 2
